ef354c18d0f840fb7c5e98a9272fe5db351db1f0
mtferreira13 <matheushsf13@gmail.com>
2025-08-14 19:23:50 -0300

    atividade 02

---
+++ exemplo_01.py
@@ -0,0 +1,8 @@
+#exercicio 2 - Criar um programa onde o usuário digite dois valores e apareça a soma
+
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+soma = valor1 + valor2
+print("A soma dos valores é:", soma)
+
+
