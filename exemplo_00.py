38a3d3c42450acc09a8486e778659cefdcc38daf
mtferreira13 <matheushsf13@gmail.com>
2025-08-14 19:23:04 -0300

    atividade 01

---
+++ exemplo_00.py
@@ -0,0 +1,7 @@
+#exercicio 1 - Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+
+nome = input("Digite seu nome: ")
+qntd_letras = len(nome)
+print("Seu nome tem:", qntd_letras,"letras")
+
+
